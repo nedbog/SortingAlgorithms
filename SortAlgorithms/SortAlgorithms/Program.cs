778cee3f52907ffa1dae3687454324e900582a0f
Bogdan Nedelcu <nedbog@gmail.com>
2018-11-05 10:33:03 +0200

    initial model commit

---
+++ SortAlgorithms/SortAlgorithms/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 7, 4, 1, 5, 3};
+
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
