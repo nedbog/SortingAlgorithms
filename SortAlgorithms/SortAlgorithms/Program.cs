d9bc124276989757b4e8bfd1a30f2323de220b7f
Bogdan Nedelcu <nedbog@gmail.com>
2018-11-05 10:45:00 +0200

    selection sort

--- SortAlgorithms/SortAlgorithms/Program.cs
+++ SortAlgorithms/SortAlgorithms/Program.cs
@@ -12,12 +12,37 @@ namespace SortAlgorithms
         {
             int[] array = { 2, 7, 4, 1, 5, 3};
 
+            Console.WriteLine("Selection Sort");
+            SelectionSort(array);
             foreach (var item in array)
             {
                 Console.Write(item + " ");
             }
+            Console.WriteLine();
+            Console.WriteLine("*** *** *** ***");
+            Console.WriteLine();
 
             Console.ReadLine();
+        }
+
+        static void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int iMin = i;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[iMin])
+                    {
+                        iMin = j;
+                    }
+                }
+
+                int temp = array[i];
+                array[i] = array[iMin];
+                array[iMin] = temp;
+            }
         }
     }
 }
